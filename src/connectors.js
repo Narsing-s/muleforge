643464749b29f34c18433e081cc456febac0df8b
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-08-17 00:36:40 +0530

    Add Snowflake JDBC dependency for generic Database connector

--- src/connectors.js
+++ src/connectors.js
@@ -1,51 +1,11 @@
 const CONNECTORS = {
-  http: {
-    name: "HTTP",
-    description: "HTTP Listener and Request",
-    maven: null,
-    namespace: "http"
-  },
-  database: {
-    name: "Database",
-    description: "Database connector",
-    maven: "org.mule.connectors:mule-db-connector",
-    namespace: "db",
-    defaultVersion: "1.14.13"
-  },
-  snowflake: {
-    name: "Snowflake",
-    description: "Snowflake database connector",
-    maven: "com.mulesoft.connectors:mule-snowflake-connector",
-    namespace: "snowflake",
-    defaultVersion: "1.0.0"
-  },
-  sftp: {
-    name: "SFTP",
-    description: "Secure file transfer",
-    maven: "org.mule.connectors:mule-sftp-connector",
-    namespace: "sftp",
-    defaultVersion: "2.5.0"
-  },
-  "ibm-mq": {
-    name: "IBM MQ",
-    description: "IBM MQ messaging",
-    maven: "com.mulesoft.connectors:mule-ibm-mq-connector",
-    namespace: "ibm-mq",
-    defaultVersion: "1.7.0"
-  },
-  "anypoint-mq": {
-    name: "Anypoint MQ",
-    description: "Anypoint MQ messaging",
-    maven: "com.mulesoft.connectors:mule-anypoint-mq-connector",
-    namespace: "anypoint-mq",
-    defaultVersion: "4.0.0"
-  },
-  "object-store": {
-    name: "Object Store",
-    description: "Mule Object Store",
-    maven: null,
-    namespace: "os"
-  }
+  http: { name: "HTTP", description: "HTTP Listener and Request", maven: null, namespace: "http" },
+  database: { name: "Database", description: "Database connector", maven: "org.mule.connectors:mule-db-connector", namespace: "db", defaultVersion: "1.14.13" },
+  snowflake: { name: "Snowflake", description: "Snowflake database connector", maven: "com.mulesoft.connectors:mule-snowflake-connector", namespace: "snowflake", defaultVersion: "1.0.0" },
+  sftp: { name: "SFTP", description: "Secure file transfer", maven: "com.mulesoft.connectors:mule-sftp-connector", namespace: "sftp", defaultVersion: "2.5.0" },
+  "ibm-mq": { name: "IBM MQ", description: "IBM MQ messaging", maven: "com.mulesoft.connectors:mule-ibm-mq-connector", namespace: "ibm-mq", defaultVersion: "1.7.0" },
+  "anypoint-mq": { name: "Anypoint MQ", description: "Anypoint MQ messaging", maven: "com.mulesoft.connectors:mule-anypoint-mq-connector", namespace: "anypoint-mq", defaultVersion: "4.0.0" },
+  "object-store": { name: "Object Store", description: "Mule Object Store", maven: null, namespace: "os" }
 };
 
 function normalizeConnector(value) {
@@ -60,19 +20,24 @@ function resolveConnectors(values = []) {
 }
 
 function buildConnectorDependencies(config = {}, versions = {}) {
-  const connectors = resolveConnectors([
+  const selected = [
     ...(config.connectors || []),
     ...(config.database && config.database.type === "snowflake" ? ["database"] : [])
-  ]);
-  return connectors.filter(c => c.maven).map(c => {
+  ];
+  const connectors = resolveConnectors(selected);
+  const dependencies = connectors.filter(c => c.maven).map(c => {
     const [groupId, artifactId] = c.maven.split(":");
-    return {
-      groupId,
-      artifactId,
-      version: versions[c.id] || c.defaultVersion,
-      classifier: "mule-plugin"
-    };
+    return { groupId, artifactId, version: versions[c.id] || c.defaultVersion, classifier: "mule-plugin" };
   });
+  if (config.database && config.database.type === "snowflake") {
+    dependencies.push({
+      groupId: "net.snowflake",
+      artifactId: "snowflake-jdbc",
+      version: versions.snowflakeJdbc || "3.20.0",
+      classifier: null
+    });
+  }
+  return dependencies;
 }
 
 module.exports = { CONNECTORS, normalizeConnector, resolveConnectors, buildConnectorDependencies };
