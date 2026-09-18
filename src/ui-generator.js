73962afb2baf436964fb5bb40bcdb450896accff
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-09-18 10:01:42 +0530

    fix: generate native Snowflake configuration

--- src/ui-generator.js
+++ src/ui-generator.js
@@ -108,12 +108,16 @@ function generateConnectivityConfigs(d) {
 function generateMuleXml(config, d) {
   const prop = name => "$" + "{" + name + "}";
   const db = config.database || {};
-  const databaseConfig = d.hasDatabase
-    ? '\n  <db:config name="Database_Config"><db:generic-connection url="' + (db.url || prop("db.url")) + '" driverClassName="' + (d.hasSnowflake ? "net.snowflake.client.jdbc.SnowflakeDriver" : "") + '" user="' + (db.user || prop("db.user")) + '" password="' + (db.password || prop("db.password")) + '" /></db:config>\n'
+  const snowflakeConfig = d.hasSnowflake
+    ? '\n  <snowflake:snowflake-config name="Snowflake_Config" doc:name="Snowflake Config"><snowflake:snowflake-connection accountName="' + (db.accountName || prop("snowflake.accountName")) + '" warehouse="' + (db.warehouse || prop("snowflake.warehouse")) + '" database="' + (db.database || prop("snowflake.database")) + '" schema="' + (db.schema || prop("snowflake.schema")) + '" user="' + (db.user || prop("snowflake.user")) + '" password="' + (db.password || prop("snowflake.password")) + '" role="' + (db.role || prop("snowflake.role")) + '"/></snowflake:snowflake-config>\n'
+    : "";
+  const databaseConfig = d.hasDatabase && !d.hasSnowflake
+    ? '\n  <db:config name="Database_Config"><db:generic-connection url="' + (db.url || prop("db.url")) + '" user="' + (db.user || prop("db.user")) + '" password="' + (db.password || prop("db.password")) + '" /></db:config>\n'
     : "";
   const connectivityConfigs = generateConnectivityConfigs(d);
-  const dbNamespace = d.hasDatabase ? ' xmlns:db="http://www.mulesoft.org/schema/mule/db"' : "";
-  const header = '<?xml version="1.0" encoding="UTF-8"?>\n<mule xmlns="http://www.mulesoft.org/schema/mule/core" xmlns:http="http://www.mulesoft.org/schema/mule/http" xmlns:ee="http://www.mulesoft.org/schema/mule/ee/core"' + dbNamespace + ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' + namespaces(d) + ' xsi:schemaLocation="' + schemas(d) + '">\n  <http:listener-config name="HTTP_Listener_config"><http:listener-connection host="0.0.0.0" port="' + prop("http.port") + '" /></http:listener-config>\n' + connectivityConfigs + databaseConfig;
+  const dbNamespace = d.hasDatabase && !d.hasSnowflake ? ' xmlns:db="http://www.mulesoft.org/schema/mule/db"' : "";
+  const snowflakeNamespace = d.hasSnowflake ? ' xmlns:snowflake="http://www.mulesoft.org/schema/mule/snowflake"' : "";
+  const header = '<?xml version="1.0" encoding="UTF-8"?>\n<mule xmlns="http://www.mulesoft.org/schema/mule/core" xmlns:http="http://www.mulesoft.org/schema/mule/http" xmlns:ee="http://www.mulesoft.org/schema/mule/ee/core"' + dbNamespace + snowflakeNamespace + ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' + namespaces(d) + ' xsi:schemaLocation="' + schemas(d) + '">\n  <http:listener-config name="HTTP_Listener_config"><http:listener-connection host="0.0.0.0" port="' + prop("http.port") + '" /></http:listener-config>\n' + connectivityConfigs + snowflakeConfig + databaseConfig;
   const flows = (config.operations || []).map(op => connectorFlow(op, d) || generateBusinessFlows({ ...config, operations: [op] }, d)).filter(Boolean);
   return header + flows.join("\n") + "</mule>\n";
 }
