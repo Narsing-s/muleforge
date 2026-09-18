39752b65f79a209c39c1dce315211c7df722e137
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-09-18 10:02:09 +0530

    fix: restore database flow after Snowflake generation change

--- src/connector-flow-generator.js
+++ src/connector-flow-generator.js
@@ -96,16 +96,32 @@ output application/json
     <set-variable variableName="httpStatus" value="\${status}" />\`);
   }
 
-  if (connector === 'database') {) {
-    const filePath = esc(op.filePath || '${sftp.filePath}');
-    const downstream = op.downstreamEndpoint
-      ? `\n    <http:request method="POST" url="${esc(op.downstreamEndpoint)}" doc:name="Send to documented downstream API" />`
-      : '';
-    return withErrorHandler(`  <flow name="${name}">
-${source(op, data, endpoint, method, status)}    <sftp:read config-ref="SFTP_Config" path="${filePath}" doc:name="Read SFTP file" />${downstream}
-    <set-variable variableName="httpStatus" value="${status}" />`);
+  if (connector === 'database') {
+    const table = esc(op.table || data.databaseTable || 'CUSTOMER');
+    const fields = op.fields || op.requestFields || [];
+    if (action === 'insert' || method === 'POST') {
+      const names = fields.length ? fields : ['name', 'email'];
+      const cols = names.map(x => esc(x.toUpperCase())).join(', ');
+      const binds = names.map(x => ':' + x).join(', ');
+      return withErrorHandler(\`  <flow name="\${name}">
+\${source(op, data, endpoint, method, status)}    <db:insert config-ref="Database_Config" doc:name="Insert \${esc(table)}">
+      <db:sql><![CDATA[INSERT INTO \${table} (\${cols}) VALUES (\${binds})]]></db:sql>
+      <db:input-parameters><![CDATA[#[{ \${params(names)} }]]]></db:input-parameters>
+    </db:insert>
+    <set-variable variableName="httpStatus" value="\${status}" />
+    <ee:transform doc:name="Response"><ee:message><ee:set-payload><![CDATA[%dw 2.0
+output application/json
+---
+{ status: "SUCCESS", data: payload }]]></ee:set-payload></ee:message></ee:transform>\`);
+    }
+    const where = op.where || (fields[0] || 'ID') + ' = :' + (fields[0] || 'id');
+    return withErrorHandler(\`  <flow name="\${name}">
+\${source(op, data, endpoint, method, status)}    <db:select config-ref="Database_Config" doc:name="Select \${esc(table)}">
+      <db:sql><![CDATA[SELECT * FROM \${table} WHERE \${esc(where)}]]></db:sql>
+      <db:input-parameters><![CDATA[#[\${JSON.stringify(op.parameters || {})}]]]></db:input-parameters>
+    </db:select>
+    <set-variable variableName="httpStatus" value="\${status}" />\`);
   }
-
   if (connector === 'anypoint-mq') {
     const destination = esc(op.destination || '${anypointmq.destination}');
     const downstream = op.downstreamEndpoint
